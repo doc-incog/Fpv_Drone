ca0e5df85cc83a26075938a95a1a068872401003
SirUnknown99 <abhijitupadhyay77@gmail.com>
2026-05-07 00:31:22 +0545

    Menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,19 +1,11 @@
 using System.Collections;
-
+using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Play()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Game");
     }
 }

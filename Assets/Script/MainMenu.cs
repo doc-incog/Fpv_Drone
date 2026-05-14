563660af9d97a680c0607eea86fa51a8714e34a8
SirUnknown99 <abhijitupadhyay77@gmail.com>
2026-05-15 00:53:16 +0545

    .
    .

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject mapPanel;
+
     public void Play()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting the application.");
+    }
+
+    public void ShowMap()
+    {
+        mainMenuPanel.SetActive(false);
+        mapPanel.SetActive(true);
+    }
+
+    public void BackFromMap()
+    {
+        mapPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 }
